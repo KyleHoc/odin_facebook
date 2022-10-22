203237f1727a043efcd8eaf8848aca8decd165ad
Kyle Hochdoerfer <kjhoch2@gmail.com>
2022-10-22 11:01:33 -0400

    Logout problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "Guy Smith", email: "anemail@gmail.com", password: "password1")
+User.create(name: "Rachel Miller", email: "thisemail@yahoo.com", password: "password2")
+User.create(name: "Marty Silver", email: "temp@gmail.com", password: "password3")
