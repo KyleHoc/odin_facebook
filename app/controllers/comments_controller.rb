9135f08fb2788c6615aa8def4a89f8653317822a
Kyle Hochdoerfer <kjhoch2@gmail.com>
2022-11-03 17:04:13 -0400

    Can't make comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,12 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @post = Post.find(params[:id])
     @comment = Comment.new(comment_params)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.post, notice: "Comment was successfully created." }
+        format.html { redirect_to @post, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
